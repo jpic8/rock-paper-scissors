c733a58063ef1a216fb9768e30821a3a050b0199
jpic8 <joshua.pickett@gmail.com>
2021-12-20 12:23:55 -0700

    initial UI implementation

--- game.js
+++ game.js
@@ -1,34 +1,48 @@
-//these variable keep track of the game progress
+//global game variables
 let playerScore = 0;
 let computerScore = 0;
 let moves = 0;
 
-//function playerSelection will prompt the user for a decision of what to play
-function playerSelection() {
-  let playerChoice = prompt("What would you like to play? rock/paper/scissors");
-  //converts string to lower case
-  let playerDecision = playerChoice.toLowerCase();
-  return playerDecision;
-}
+//DOM elements
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-//function computerPlay will randomly return either 'rock', 'paper', or 'scissors'
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
+const roundResult = document.querySelector("#roundResult");
+const playerResult = document.querySelector("#playerResult");
+const computerResult = document.querySelector("#computerResult");
+const scoreBoard = document.querySelector("#scoreBoard");
+const gameReset = document.querySelector("#gameReset");
+
+//listens to player choices by button click and starts singleRound function
+rock.addEventListener("click", () => {
+  singleRound("rock");
+});
+paper.addEventListener("click", () => {
+  singleRound("paper");
+});
+scissors.addEventListener("click", () => {
+  singleRound("scissors");
+});
+
+//function computerSelection will randomly return either 'rock', 'paper', or 'scissors'
 function computerSelection() {
   const choices = ["rock", "paper", "scissors"];
-  const randomNumber = Math.floor(Math.random() * 3);
-  let decision = choices[randomNumber];
-  return decision;
+  const randomNumber = Math.floor(Math.random() * choices.length);
+  return choices[randomNumber];
 }
 
-//function singleRound plays a round of RPS between user and computer
-//it will increase playerScore or computerScore depending on who wins
-//a tie or invalid user entry does not count as a move in a game of 5
+/*function singleRound plays a round of RPS between user and computer
+it will increase playerScore or computerScore depending on who wins
+a tie or invalid user entry does not count as a move in a game of 5*/
 function singleRound(player, computer) {
-  const playerRound = playerSelection();
+  const playerRound = player;
   const computerRound = computerSelection();
 
   if (playerRound == computerRound) {
-    let result = "It's a tie, try again!";
-    console.log(result);
+    roundResult.textContent = "It's a tie, try again!";
   } else if (
     (playerRound == "rock" && computerRound == "scissors") ||
     (playerRound == "scissors" && computerRound == "paper") ||
@@ -36,7 +50,7 @@ function singleRound(player, computer) {
   ) {
     playerScore += 1;
     moves += 1;
-    console.log(`You win! ${playerRound} beats ${computerRound}`);
+    roundResult.textContent = `You win! ${playerRound} beats ${computerRound}`;
   } else if (
     (playerRound == "scissors" && computerRound == "rock") ||
     (playerRound == "paper" && computerRound == "scissors") ||
@@ -44,22 +58,26 @@ function singleRound(player, computer) {
   ) {
     computerScore += 1;
     moves += 1;
-    console.log(`You lose! ${computerRound} beats ${playerRound}`);
+    roundResult.textContent = `You lose! ${computerRound} beats ${playerRound}`;
   }
-
-  console.log(`Player had ${playerRound}`);
-  console.log(`Computer had ${computerRound}`);
-  return moves;
+  playerResult.textContent = `player had ${playerRound}`;
+  computerResult.textContent = `computer had ${computerRound}`;
+  scoreKeeper(moves, playerScore, computerScore);
 }
 
-//function game will begin the play of the game and return the total moves and scores
-function game() {
-  while (moves < 5) {
-    singleRound();
-    console.log(`Total moves: ${moves}`);
-    console.log(`Player score: ${playerScore}`);
-    console.log(`Computer score ${computerScore}`);
+//function scoreKeeper will track the number of wins and declare the winner after 5
+function scoreKeeper(moves, playerScore, computerScore) {
+  if (moves < 5) {
+    scoreBoard.textContent = `Game ${moves} | player score ${playerScore} | computer score ${computerScore}`;
+  } else if ((moves = 5)) {
+    input.style.display = "none";
+    if (playerScore > computerScore) {
+      roundResult.textContent = "You WIN! Congrats!";
+    } else {
+      roundResult.textContent = "Computer wins, try harder next time!";
+    }
+    scoreBoard.textContent = `Game ${moves} | player score ${playerScore} | computer score ${computerScore}`;
+    gameReset.innerHTML =
+      '<button onclick="location.reload()"> Play again? </button>';
   }
 }
-
-game();
